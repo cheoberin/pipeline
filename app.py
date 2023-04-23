65ae96516d1030f72b9930abe205e91bad438097
Airton Manfrini Junior <46052483+cheoberin@users.noreply.github.com>
2023-04-22 22:49:25 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+print(f"A soma de {a} e {b} é igual a {soma}")
+print(f"A subtração de {a} e {b} é igual a {subtracao}")
+print(f"A multiplicação de {a} e {b} é igual a {multiplicacao}")
+print(f"A divisão de {a} por {b} é igual a {divisao}")
